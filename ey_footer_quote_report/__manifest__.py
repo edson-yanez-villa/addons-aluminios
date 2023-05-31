61a45e940a1eefe32beca76c5b58c55ff8b7f9a0
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2023-05-31 17:03:41 -0400

    Add new module to modify the footer of a quote report

---
+++ ey_footer_quote_report/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Footer updater in qoute report",
+    'summary': """Update footer in qoute report""",
+    'description': """Update footer in qoute report.""",
+    'author': "",
+    'website': "",
+    'license': "AGPL-3",
+    'category': "",
+    'version': "10.0.1.0",
+    'depends': ["base", "report"],
+    'data': [
+        "views/external_layout_footer.xml",
+    ],
+    "images": [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
