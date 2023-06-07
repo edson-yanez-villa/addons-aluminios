135eb1639d75dce6ba22e61170a2b8129f37ef3b
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2023-06-07 11:40:20 -0400

    Update order method to cancel button

--- ey_bo_cancel_button_group/models/sale_order.py
+++ ey_bo_cancel_button_group/models/sale_order.py
@@ -8,7 +8,8 @@ class SaleOrder(models.Model):
     def _can_cancel_button(self):
         for record in self:
             pickings = record.picking_ids.filtered(lambda picking: picking.state == 'done' )
-            if record.state == 'sale' and len(pickings) > 0:
+            invoices = record.invoice_ids.filtered(lambda invoice: invoice.state in ['open', 'paid'])
+            if record.state == 'sale' and (len(pickings) > 0 or len(invoices) > 0):
                 record.can_cancel = self.env.user.has_group('ey_bo_cancel_button_group.group_cancel_button')
             else:
                 record.can_cancel = record.state in ['sale', 'draft', 'sent'] and len(pickings) == 0
