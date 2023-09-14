22e3e74052990e68627d05c34afc14bc97c0f618
Moises Edson Yanez Villa <yanezvilla.edson@gmail.com>
2023-09-13 21:54:11 -0400

    Add category manager module

---
+++ ey_bo_category_manager/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Administrador de Categorias',
+    'version': '10.0.1.0',
+    'description': 'Managment categories to archive or unarchive',
+    'summary': 'Managment categories to archive or unarchive',
+    'author': 'Moises Edson Yanez Villa',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'base',
+        'product'
+    ],
+    'data': [
+        'views/product_category_active.xml'
+    ],
+    'auto_install': False,
+    'application': False,
+}
